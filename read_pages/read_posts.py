c738b1009d50ecd75db16586308e82c2bfe84986
Ritab <ritabrata.saha@gmail.com>
2020-08-10 19:56:32 +0000

    Initial Commit

---
+++ read_pages/read_posts.py
@@ -0,0 +1,29 @@
+import json
+import facebook
+from  utility import get_accessToken 
+import pandas as pd
+
+def get_posts():
+
+    print()
+    graph =  facebook.GraphAPI(access_token=get_accessToken())
+    
+    # app_id = '611417473073068'
+    # app_secret = '39b76b9499d3623d4a31b6ed9a200c7d'
+    # graph = facebook.GraphAPI()
+    # access_token = graph.get_app_access_token(app_id, app_secret)
+
+    posts = graph.get_object(id = '671278940149620',fields = 'posts')
+
+    print(json.dumps(posts, indent=4))
+
+    print('Page id  = ', posts['id'] + '\n\n')
+    for items in posts['posts']['data']:
+        print(items)
+
+              
+def main():
+    get_posts()
+
+if __name__ == "__main__":
+    main()
---
+++ read_pages/utility.py
@@ -0,0 +1,6 @@
+import facebook
+
+def get_accessToken():
+    token = 'EAAIsFLlKv6wBACKKecUibIjEGW3idiKJ84BFUGhCZA1vlWZBZA4kXesS8HSo2BEcVoeKm6GOjOgVrUM1g4hKbKjxCCNJa6ZBfYobolYtmpHTa7v9H41lF1iZCi7ziMDBDOHA89loZBVB50PSXv5Vo3S021q0GveDMveHRAS2wfuAZDZD'
+    return token
+
